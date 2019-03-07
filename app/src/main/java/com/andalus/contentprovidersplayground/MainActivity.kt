c00874ec9ae1a334d732e10cc59a5708fc8fcf4e
Abo_Med7at <abomed7at55@gmail.com>
2019-03-07 16:09:30 +0200

    Added Runtime Permission

--- app/src/main/java/com/andalus/contentprovidersplayground/MainActivity.kt
+++ app/src/main/java/com/andalus/contentprovidersplayground/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.andalus.contentprovidersplayground
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,10 +18,47 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val CONTACTS_PERMISSION_REQUEST_CODE = 5
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.READ_CONTACTS), CONTACTS_PERMISSION_REQUEST_CODE
+                )
+            } else {
+                runTheApp()
+            }
+        } else {
+            runTheApp()
+        }
+
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CONTACTS_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                runTheApp()
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    getString(R.string.permission_error),
+                    Toast.LENGTH_LONG
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun runTheApp() {
         val rvContacts = rvContacts
 
         val adapter = ContactsAdapter()
@@ -31,8 +73,7 @@ class MainActivity : AppCompatActivity() {
         viewModel.liveData.observe(this, Observer {
             adapter.data = it
             adapter.notifyDataSetChanged()
-            Log.d("Observer","works")
+            Log.d("Observer", "works")
         })
     }
-
 }
